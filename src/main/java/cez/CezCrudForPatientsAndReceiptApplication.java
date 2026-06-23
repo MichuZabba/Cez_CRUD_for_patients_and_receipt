988208483aa4e2ac5febf97f5bbd607ee15381ad
michal <michu.romanczuk@gmail.com>
2026-06-23 13:48:43 +0200

    Innitial commit

---
+++ src/main/java/cez/CezCrudForPatientsAndReceiptApplication.java
@@ -0,0 +1,13 @@
+package cez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CezCrudForPatientsAndReceiptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CezCrudForPatientsAndReceiptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cez/CezCrudForPatientsAndReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package cez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CezCrudForPatientsAndReceiptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
